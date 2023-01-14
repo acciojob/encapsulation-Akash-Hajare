84387d48401ac850886b5c36d975135c8ef045b5
Akash-Hajare <akashhajare97@gmail.com>
2023-01-14 14:50:15 +0530

    Implementation done

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,6 +1,24 @@
 package com.driver;
 
 public class RWOnly {
-    private int name ;
+    private String name;
+    private String readOnly="RCERT";
+    private String writeOnly;
+    public String getReadOnly() {
+        return readOnly;
+    }
 
+    public void setWriteOnly(String writeOnly) {
+        this.writeOnly = writeOnly;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
 }
+    
+    
+
